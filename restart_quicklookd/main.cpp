8a60a936cf70ad937e95cc23460daa1168fa4e8a
Alan Gardner <alangardner97520@gmail.com>
2022-11-28 02:00:15 -0500

    Add #define for logging

--- restart_quicklookd/main.cpp
+++ restart_quicklookd/main.cpp
@@ -9,12 +9,16 @@
 #include <chrono>
 #include <ctime>
 
+#define ENABLE_LOGGING //comment out to disable logging
+
 int main(int argc, const char * argv[]) {
     
+#ifdef ENABLE_LOGGING
     auto start = std::chrono::system_clock::now();
     std::time_t curr_time = std::chrono::system_clock::to_time_t(start);
-    
     std::cerr << "Restarting Quicklook at: " << std::ctime(&curr_time);
+#endif
+    
     system("killall -KILL QuickLookUIService");
     
     return 0;
